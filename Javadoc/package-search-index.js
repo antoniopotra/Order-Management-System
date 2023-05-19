9006059357f103621b031afd49f79a0a339de653
antoniopotra <antonio.potra@gmail.com>
2023-05-19 14:31:39 +0300

    javadoc

---
+++ Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example"},{"l":"org.example.businessLogic"},{"l":"org.example.connection"},{"l":"org.example.dataAccess"},{"l":"org.example.model"},{"l":"org.example.presentation"}];updateSearchResults();
